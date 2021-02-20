929a55517d51ab8f14192a71b78d1e0eefb224ae
Anvil Zero <67426829+anvilzero@users.noreply.github.com>
2021-02-20 07:47:18 -0500

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -2,6 +2,9 @@
 # chmod a+x on this file, then run:$ sh install_portainer.sh
 # Installs Portainer to port 9000
 # Sets admin password, loads templates and logo from Github
+# Create a password hash, but double escape $ via $$:
+# docker run --rm httpd:2.4-alpine htpasswd -nbB admin your_password | cut -d ":" -f 2
+# Password is only set on first start, re-create volume to set new password
 
 docker run -d \
   -p 9000:9000 \
@@ -10,6 +13,6 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data portainer/portainer \
   -H unix:///var/run/docker.sock \
-  --admin-password='$2y$05$ToHtSrp3RiCvch.DQd3G2OgUFbeiRLR1eYh5Fcv5F9ZKJDBw.uCti' \
+  --admin-password='$$2y$$05$$ToHtSrp3RiCvch.DQd3G2OgUFbeiRLR1eYh5Fcv5F9ZKJDBw.uCti' \
   --templates "https://raw.githubusercontent.com/anvilzero/ubuntu-20.10-installer/master/portainer-templates.json" \
   --logo "https://raw.githubusercontent.com/anvilzero/ubuntu-20.10-installer/master/logo.svg"
